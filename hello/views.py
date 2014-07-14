9c35f9e846b7d1f3c596f59613c7469209c62e62
unknown <dhanwav@AELBI00008W.lbi.local>
2014-07-14 18:23:51 +0400

    Got users running,  try /login

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,16 @@
 from django import http
 
 def home(request):
-    return http.HttpResponse('Hello World!')
+    return http.HttpResponse('Hello World! Please try /login for some magic')
+
+def login(request):
+	from google.appengine.api import users
+	user = users.get_current_user()
+
+	if user:
+		greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' % (user.nickname(), users.create_logout_url('/')))
+	
+	else:
+		greeting = ('<a href="%s">Sign in or register</a>.' % users.create_login_url('/'))
+	
+	return http.HttpResponse('<html><body>%s</body></html>' % greeting)
