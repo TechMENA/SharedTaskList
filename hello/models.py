f93400c09fcc3f973d56b5531056e30e5ff8cfa8
danny crasto <danwald79@gmail.com>
2014-07-14 18:54:58 +0400

    added models

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,10 @@
 from google.appengine.ext import ndb
 
 # Create your models here.
+
+class Task(ndb.Model):
+    name = ndb.StringProperty()
+    done = ndb.BooleanPropterty()
+
+class TaskList(ndb.Model):
+    tasks = ndb.StructuredProperty(Task, repeated=True)
