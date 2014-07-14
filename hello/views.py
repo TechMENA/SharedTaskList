b9c067a783ca1686412ccacf89ba204769d91252
danny crasto <danwald79@gmail.com>
2014-07-14 22:05:39 +0400

    changed LoginView to ClassedBasedView

--- hello/views.py
+++ hello/views.py
@@ -10,17 +10,16 @@ class HomeView(View):
         return HttpResponse('Hello World! Please try /login for some magic')
 
 
-def LoginView(request):
-    user = users.get_current_user()
+class LoginView(View):
+    def get(self, request):
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' % users.create_login_url('/'))
 
-    if user:
-        greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
-                    (user.nickname(), users.create_logout_url('/')))
-
-    else:
-        greeting = ('<a href="%s">Sign in or register</a>.' % users.create_login_url('/'))
-
-    return HttpResponse('<html><body>%s</body></html>' % greeting)
+        return HttpResponse('<html><body>%s</body></html>' % greeting)
 
 
 class TaskListView(View):
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
-from hello.views import HomeView, TaskListView
+from hello.views import LoginView, HomeView, TaskListView
 
 urlpatterns = patterns('',
-                       url(r'^login/$', 'hello.views.LoginView'),
+                       url(r'^login/$', LoginView.as_view()),
                        url(r'^lists/$', TaskListView.as_view()),
                        url(r'^$', HomeView.as_view()),
                        )
